7a8bd08c5d84a5d0fbef5609ec02cf94968b5770
Vanessa Su <vnessa.su@gmail.com>
2021-09-23 14:04:48 -0400

    :hammer: Add bcrypt password hashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 // SIGN UP
 // POST /api/signup
 router.post("/signup", (req, res, next) => {
-    User.create(req.body)
+    bcrypt
+        .hash(req.body.password, 10)
+        // return a new object with the email and hashed password
+        .then((hash) =>
+            // when returning an object with fat arrow syntax, we
+            // need to wrap the object in parentheses so JS doesn't
+            // read the object curly braces as the function block
+            ({
+                email: req.body.email,
+                password: hash,
+            })
+        )
+        // create user with provided email and hashed password
+        .then((user) => User.create(user))
+        // send the new user object back with status 201, but `hashedPassword`
+        // won't be send because of the `transform` in the User model
         .then((user) => res.status(201).json(user))
+        // pass any errors along to the error handler
         .catch(next);
 });
 
