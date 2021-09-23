a3df2eb065b223bd3bd9fa520350dbae1418f2c6
Vanessa Su <vnessa.su@gmail.com>
2021-09-23 13:26:32 -0400

    :hammer: Add error handling in index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/jobs", jobsController);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).send(message);
+});
 
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
