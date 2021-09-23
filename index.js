11f4534670968f56cc99e52749fecdadde0b203a
Vanessa Su <vnessa.su@gmail.com>
2021-09-23 11:35:01 -0400

    :hammer: Add jobs controller and routes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const jobsController = require("./controllers/jobs");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/jobs", jobsController);
 
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
