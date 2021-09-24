0e5c94a2a0a4d5d3c9e86c750c08af39c8b11fdf
Vanessa Su <vnessa.su@gmail.com>
2021-09-23 22:27:40 -0400

    :hammer: Alter data seed to seed all jobs with given user data

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,35 @@
+const User = require("../models/User");
 const Job = require("../models/Job");
 const seedData = require("./seeds.json");
 
+const getUser = async () => {
+    try {
+        if (!process.argv[2]) {
+            throw new Error(
+                "To seed the database provide an email address for an existing user"
+            );
+        }
+        const user = await User.findOne({ email: process.argv[2] });
+        if (!user) {
+            throw new Error("No matching user found!");
+        }
+        return user;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 Job.deleteMany()
-    .then(() => Job.insertMany(seedData))
-    .then((result) => console.log(result))
-    .catch((err) => console.error(err))
-    .finally(process.exit);
+    .then(getUser)
+    .then((user) => {
+        const seedDataWithOwner = seedData.map((job) => {
+            job.owner = user._id;
+            return job;
+        });
+        return Job.insertMany(seedDataWithOwner);
+    })
+    .then(console.log)
+    .then(console.error)
+    .finally(() => {
+        process.exit();
+    });
